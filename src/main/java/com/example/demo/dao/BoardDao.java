3f17d3e6c52b0ab1714796be1dd953ac27473e15
LDH <LDH@LG-gram>
2020-04-03 12:57:09 +0900

    워크1 재수정 - 업데이트 추가

--- src/main/java/com/example/demo/dao/BoardDao.java
+++ src/main/java/com/example/demo/dao/BoardDao.java
@@ -1,5 +1,10 @@
 package com.example.demo.dao;
 
 public class BoardDao {
-
+	public void listBoard() {
+		System.out.println("list board");
+	}
+	public void insertBoard() {
+		System.out.println("인서트 테스트");
+	}
 }
